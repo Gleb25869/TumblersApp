066a4b916fcdccd342c81bd5286915e233b316f8
Goryachev Gleb Vladimirovich <glebgoryaschev@icloud.com>
2026-02-15 23:39:34 +0300

    UPD

--- script.js
+++ script.js
@@ -25,13 +25,6 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
   });
 });
 
-const cursor = document.getElementById("cursor");
-
-document.addEventListener("mousemove", e => {
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
-});
-
 const cards = document.querySelectorAll(".card");
 
 const observer = new IntersectionObserver(
